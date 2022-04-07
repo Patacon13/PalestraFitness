bc0aed2d82810866a9af1bd0cf945252e70524de
Tomas Assenza <tomyassenza@gmail.com>
2022-04-06 22:12:56 -0300

    First commit

---
+++ app/src/main/java/com/example/palestrafitness/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.palestrafitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void aceptarIngreso(View view) {
+        System.out.println("presionado!");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botonAceptar = findViewById(R.id.botonAceptar);
+
+        botonAceptar.setBackgroundColor(Color.rgb(255,165,0));
+
+        Intent intent = new Intent(this, AdministrationActivity.class);
+        startActivity(intent);
+    }
+}
