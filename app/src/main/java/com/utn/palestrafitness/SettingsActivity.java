71f334e2eae3e87757e93016a967e38667d48d38
Tomas Assenza <tomyassenza@gmail.com>
2022-06-26 20:07:16 -0300

    Bugfix

--- app/src/main/java/com/utn/palestrafitness/SettingsActivity.java
+++ app/src/main/java/com/utn/palestrafitness/SettingsActivity.java
@@ -1,9 +1,17 @@
 package com.utn.palestrafitness;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.utn.palestrafitness.lib.Rutina;
 
 public class SettingsActivity extends AppCompatActivity {
 
